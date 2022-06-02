dc009684caabbe5a6d861f256958b9612938eda0
sergio rodriguez <sarv9208@hotmail.com>
2022-06-02 12:05:05 -0500

    Corrección de modal editar

--- src/app/components/pregunta/pregunta.component.ts
+++ src/app/components/pregunta/pregunta.component.ts
@@ -3,6 +3,8 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { Pregunta } from 'src/app/models/pregunta';
 import { PreguntaService } from 'src/app/services/pregunta.service';
 import { ToastrService } from 'ngx-toastr';
+import { Opcion } from 'src/app/models/opcion';
+import { concat } from 'rxjs';
 
 @Component({
   selector: 'app-pregunta',
@@ -12,12 +14,26 @@ import { ToastrService } from 'ngx-toastr';
 export class PreguntaComponent implements OnInit {
 
   listPregunta: Pregunta[];
+  seleccionEditar: Pregunta;
+
+  regNumeros = '^[0-9]|[1-2][0-9]$';
+  regTextoUnaLinea = '^[a-zA-ZÀ-ÿ\u00f1\u00d1\u0020-\u003f\u00bf\u00a1]+';
 
   public cargando = false;
 
   filtrarForm = new FormGroup({
-    numPregunta: new FormControl('', [Validators.pattern('^[0-9]|[1-2][0-9]$')]),
-    enunciado: new FormControl(),
+    numPregunta: new FormControl('', [Validators.pattern(this.regNumeros)]),
+    enunciado: new FormControl(''),
+  });
+
+  editarPreguntaForm = new FormGroup({
+    numePregunta: new FormControl('', [Validators.pattern(this.regNumeros)]),
+    enumPregunta: new FormControl('', [Validators.pattern(this.regTextoUnaLinea)]),
+    opcionA: new FormControl('', [Validators.pattern(this.regTextoUnaLinea)]),
+    opcionB: new FormControl('', [Validators.pattern(this.regTextoUnaLinea)]),
+    opcionC: new FormControl('', [Validators.pattern(this.regTextoUnaLinea)]),
+    opcionD: new FormControl('', [Validators.pattern(this.regTextoUnaLinea)]),
+    urlImg: new FormControl({value: '', disabled: true})
   });
 
   constructor(
@@ -25,6 +41,11 @@ export class PreguntaComponent implements OnInit {
     private toastrService: ToastrService,
   ) {
     this.listPregunta = [];
+    this.seleccionEditar = new Pregunta();
+    this.seleccionEditar.opciones[0] = new Opcion();
+    this.seleccionEditar.opciones[1] = new Opcion();
+    this.seleccionEditar.opciones[2] = new Opcion();
+    this.seleccionEditar.opciones[3] = new Opcion();
   }
 
   ngOnInit(): void {
@@ -57,6 +78,11 @@ export class PreguntaComponent implements OnInit {
   limpiar(): void {
     this.filtrarForm.get('numPregunta')?.setValue("");
     this.filtrarForm.get('enunciado')?.setValue("");
+    this.filter();
   }
 
+  seleccionarEditar(pregunta: Pregunta){
+    this.editarPreguntaForm.get('numePregunta')?.disable();
+    this.seleccionEditar = pregunta;
+  }
 }
