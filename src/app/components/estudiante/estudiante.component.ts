e2311200c92c221bc9ecdcfd8181477cb786cc34
sergio rodriguez <sarv9208@hotmail.com>
2022-06-07 16:41:48 -0500

    Gestion de estudiantes

--- src/app/components/estudiante/estudiante.component.ts
+++ src/app/components/estudiante/estudiante.component.ts
@@ -1,6 +1,7 @@
 import { DatePipe } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { ModalDismissReasons, NgbModal, NgbModalConfig } from '@ng-bootstrap/ng-bootstrap';
 import { ToastrService } from 'ngx-toastr';
 import { Estudiante } from 'src/app/models/estudiante';
 import { Respuesta } from 'src/app/models/respuesta';
@@ -23,11 +24,14 @@ export class EstudianteComponent implements OnInit {
   cargando = false;
 
   seleccionEditar: Estudiante;
+  seleccionEliminar: Estudiante;
   seleccionRespuesta: Respuesta;
   notaRespuesta: string = '0.0';
   fechaRespuesta: Date;
 
   fechaActual!: Date;
+
+  closeResult = '';
 
   filtrarForm = new FormGroup({
     nombre: new FormControl('', [Validators.pattern(this.regNombre)]),
@@ -42,16 +46,31 @@ export class EstudianteComponent implements OnInit {
     fecha: new FormControl()
   });
 
+  agregarEstudianteForm = new FormGroup({
+    nombre: new FormControl('', [Validators.pattern(this.regNombre)]),
+  });
+
+  eliminarEstudianteForm = new FormGroup({
+    idEstudiante: new FormControl(''),
+    nombre: new FormControl('', [Validators.pattern(this.regNombre)]),
+  });
+
   constructor(
     private estudianteService: EstudianteService,
     private toastrService: ToastrService,
     private dateFormat: DatePipe,
+    private modalService: NgbModal,
+    config: NgbModalConfig
   ) {
     this.listaEstudiante = [];
     this.listaRespuesta = [];
     this.seleccionEditar = new Estudiante();
+    this.seleccionEliminar = new Estudiante();
     this.seleccionRespuesta = new Respuesta();
     this.fechaRespuesta = new Date();
+
+    config.backdrop = 'static';
+    config.keyboard = false;
   }
 
   ngOnInit(): void {
@@ -66,30 +85,110 @@ export class EstudianteComponent implements OnInit {
     const nombre = this.filtrarForm.controls['nombre'].value;
     const fechaEntrega = this.filtrarForm.controls['fechaEntrega'].value;
 
-    console.log(fechaEntrega);
-
     this.estudianteService.filtrarEstudiante(nombre ? nombre : null, fechaEntrega).subscribe(resp => {
       this.listaEstudiante = resp.data;
       if (resp.success) {
-        this.toastrService.success(resp.message, 'Proceso fallido', { timeOut: 5000, closeButton: true });
+        this.toastrService.success(resp.message, 'Proceso exitoso', { timeOut: 4000, closeButton: true });
         this.cargando = false;
       } else {
-        this.toastrService.error(resp.message, 'Proceso fallido', { timeOut: 5000, closeButton: true });
+        this.toastrService.error(resp.message, 'Proceso fallido', { timeOut: 4000, closeButton: true });
         this.cargando = false;
       }
-    })
+    }, error => {
+      this.toastrService.error(error.message, 'Proceso fallido', { timeOut: 4000, closeButton: true });
+      this.cargando = false;
+    });
   }
 
-  seleccionarEditar(estudiante: Estudiante) {
+  agregarEstudiante() {
+
+    this.cargando = true;
+
+    const newEstudiante = new Estudiante();
+    newEstudiante.nombre = this.agregarEstudianteForm.controls['nombre'].value;
+    newEstudiante.idusuario = '-1';
+
+    this.estudianteService.agregar(newEstudiante).subscribe(resp => {
+      if (resp.success) {
+        this.toastrService.success(resp.message, 'Proceso exitoso', { timeOut: 4000, closeButton: true });
+        this.cargando = false;
+        this.modalService.dismissAll('Save click');
+        this.filtrar();
+      } else {
+        this.toastrService.error(resp.message, 'Proceso fallido', { timeOut: 4000, closeButton: true });
+        this.cargando = false;
+      }
+    }, error => {
+      this.toastrService.error(error.message, 'Proceso fallido', { timeOut: 4000, closeButton: true });
+      this.cargando = false;
+    });
+  }
+
+  editarEstudiante(){
+
+    this.cargando = true;
+    const actualizarEst = new Estudiante();
+
+    actualizarEst.idusuario = this.editarEstudianteForm.controls['idEstudiante'].value;
+    actualizarEst.nombre = this.editarEstudianteForm.controls['nombre'].value;
+
+    this.estudianteService.actualizar(actualizarEst).subscribe( resp => {
+      if(resp.success){
+        this.toastrService.success(resp.message, 'Proceso exitoso', {timeOut: 4000, closeButton: true});
+        this.cargando = false;
+        this.modalService.dismissAll('Save click');
+        this.filtrar();
+      }else{
+        this.toastrService.error(resp.message, 'Proceso fallido', {timeOut: 4000, closeButton: true});
+        this.cargando = false;
+      }
+    }, error => {
+      this.toastrService.error(error.message, 'Proceso fallido', {timeOut: 4000, closeButton: true});
+      this.cargando = false;
+    });
+  }
+
+  eliminarEstudiante(estudiante: any){
+
+    this.cargando = true;
+
+    this.estudianteService.eliminar(estudiante).subscribe( resp => {
+      if(resp.success){
+        this.toastrService.success(resp.message, 'Ptoceso exitoso', {timeOut: 4000, closeButton: true});
+        this.cargando = false;
+        this.modalService.dismissAll('Save click');
+        this.filtrar();
+      }else{
+        this.toastrService.error(resp.message, 'Ptoceso fallido', {timeOut: 4000, closeButton: true});
+        this.cargando = false;
+      }
+    }, error => {
+      this.toastrService.error(error.message, 'Ptoceso exitoso', {timeOut: 4000, closeButton: true});
+      this.cargando = false;
+    });
+  }
+
+  seleccionarEditar(estudiante: Estudiante, contentEdit?: any) {
     this.seleccionEditar = estudiante;
     this.editarEstudianteForm.get('idEstudiante')?.disable();
     this.editarEstudianteForm.get('nota')?.disable();
     this.editarEstudianteForm.get('fecha')?.disable();
+
+    this.editarEstudianteForm.get('idEstudiante')?.setValue(this.seleccionEditar.idusuario);
+    this.editarEstudianteForm.get('nombre')?.setValue(this.seleccionEditar.nombre);
+
+    this.open(contentEdit);
   }
 
-  limpiar() {
-    this.filtrarForm.get('nombre')?.value(null);
-    this.filtrar();
+  seleccionarEliminar(estudiante: Estudiante, contentEliminar?: any) {
+    this.seleccionEliminar = estudiante;
+    this.eliminarEstudianteForm.get('idEstudiante')?.disable();
+    this.eliminarEstudianteForm.get('nombre')?.disable();
+
+    this.eliminarEstudianteForm.get('idEstudiante')?.setValue(this.seleccionEliminar.idusuario);
+    this.eliminarEstudianteForm.get('nombre')?.setValue(this.seleccionEliminar.nombre);
+
+    this.open(contentEliminar);
   }
 
   seleccionarRespuesta() {
@@ -103,7 +202,63 @@ export class EstudianteComponent implements OnInit {
     }
   }
 
-  borrarFecha(){
+  borrarFecha() {
     this.filtrarForm.get('fechaEntrega')?.setValue(null);
   }
+
+  limpiar(): void {
+    console.log("limpiando");
+    this.filtrarForm.get('nombre')?.setValue('');
+    this.filtrarForm.get('fechaEntrega')?.value(null);
+    this.filtrar();
+  }
+
+  open(content: any) {
+
+    this.modalService.open(content, { ariaLabelledBy: 'modal-basic-title', size: 'lg', backdropClass: 'light-blue-backdrop' }).result.then((result) => {
+      this.closeResult = `Closed with: ${result}`;
+    },  (reason) => {
+      this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
+    });
+  }
+
+  private getDismissReason(reason: any): string {
+    if (reason === ModalDismissReasons.ESC) {
+      return 'by pressing ESC';
+    } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
+      return 'by clicking on a backdrop';
+    } else {
+      return `with: ${reason}`;
+    }
+  }
+
+  resetearFiltrarForm(){
+    this.filtrarForm.get('nombre')?.setValue('');
+    this.filtrarForm.get('fechaEntrega')?.setValue(null);
+  }
+
+  resetearEditarEstudianteForm(){
+    this.editarEstudianteForm.get('idEstudiante')?.setValue('');
+    this.editarEstudianteForm.get('nombre')?.setValue('');
+    this.editarEstudianteForm.get('respuesta')?.setValue('');
+    this.editarEstudianteForm.get('nota')?.setValue('0');
+    this.editarEstudianteForm.get('fecha')?.setValue(null);
+    this.seleccionEditar = new Estudiante();
+    this.notaRespuesta = '0';
+    this.fechaRespuesta = new Date();
+    this.seleccionEditar = new Estudiante();
+    this.modalService.dismissAll('Close click');
+  }
+
+  resetearAgregarEstudianteForm(){
+    this.agregarEstudianteForm.get('nombre')?.setValue('');
+    this.modalService.dismissAll('Close click');
+  }
+
+  resetearEliminarEstudianteForm(){
+    this.agregarEstudianteForm.get('idEstudiante')?.setValue('');
+    this.agregarEstudianteForm.get('nombre')?.setValue('');
+    this.modalService.dismissAll('Close click');
+  }
+
 }
--- src/app/services/estudiante.service.ts
+++ src/app/services/estudiante.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { GlobalConstant } from '../utils/constants/global.constants';
@@ -7,7 +7,6 @@ import { GlobalConstant } from '../utils/constants/global.constants';
   providedIn: 'root'
 })
 export class EstudianteService {
-
   constructor(
     private http: HttpClient
   ) { }
@@ -15,12 +14,36 @@ export class EstudianteService {
   public filtrarEstudiante(nombre: any, fechaEntrega: any): Observable<any> {
 
     const URL = GlobalConstant.URL_ENDPOINT + GlobalConstant.URL_ESTUDIANTE_FILTRO;
-    console.log("Peticion", nombre, URL);
+
     let params = '';
 
     params = nombre ? (params.length > 0 ? params.concat('&nombre=').concat(nombre) : params.concat('?nombre=').concat(nombre)) : params;
     params = fechaEntrega ? (params.length > 0 ? params.concat('&fecha=').concat(fechaEntrega) : params.concat('?fecha=').concat(fechaEntrega)) : params;
 
     return this.http.get<any>(URL + params);
+  }
+
+  public agregar(newEstudiante: any ): Observable<any> {
+    const URL = GlobalConstant.URL_ENDPOINT + GlobalConstant.URL_ESTUDIANTE;
+
+    return this.http.post(URL, newEstudiante);
+  }
+
+  public actualizar(actualizarEst: any): Observable<any> {
+    const URL = GlobalConstant.URL_ENDPOINT + GlobalConstant.URL_ESTUDIANTE;
+
+    return this.http.put<any>(URL, actualizarEst);
+  }
+
+  public eliminar(estudiante: any ): Observable<any>{
+    const URL = GlobalConstant.URL_ENDPOINT + GlobalConstant.URL_ESTUDIANTE;
+    const options = {
+      headers: new HttpHeaders({
+        'Content-Type': 'application/json',
+      }),
+      body: estudiante,
+    };
+
+    return this.http.delete<any>(URL, options);
   }
 }
