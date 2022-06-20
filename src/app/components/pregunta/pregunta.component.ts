a5de103c8591692da5879cb4e84f5fe02520b7c1
SERGIO RODRÍGUEZ <SARV9208@hotmail.com>
2022-06-19 19:26:40 -0500

    Creacion de modal crear pregunta

--- src/app/components/pregunta/pregunta.component.ts
+++ src/app/components/pregunta/pregunta.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, NgModule, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { Pregunta } from 'src/app/models/pregunta';
 import { PreguntaService } from 'src/app/services/pregunta.service';
 import { ToastrService } from 'ngx-toastr';
 import { Opcion } from 'src/app/models/opcion';
+import { ModalDismissReasons, NgbModal, NgbModalConfig } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-pregunta',
@@ -16,9 +17,12 @@ export class PreguntaComponent implements OnInit {
   seleccionEditar: Pregunta;
 
   regNumeros = '^[0-9]|[1-2][0-9]$';
+  regNombre = '^[a-zA-ZÀ-ÿ][a-zA-ZÀ-ÿ\u00f1\u00d1\u0020-\u003f\u00bf\u00a1]+[a-zA-ZÀ-ÿ]$';
   regTextoUnaLinea = '^[a-zA-ZÀ-ÿ\u00f1\u00d1\u0021-\u003f\u00bf\u00a1].[a-zA-ZÀ-ÿ\u00f1\u00d1\u0020-\u003f\u00bf\u00a1]+[a-zA-ZÀ-ÿ\u00f1\u00d1\u0021-\u003f\u00bf\u00a1]$';
 
   cargando = false;
+  
+  closeResult = '';
 
   filtrarForm = new FormGroup({
     numPregunta: new FormControl('', [Validators.pattern(this.regNumeros)]),
@@ -36,7 +40,6 @@ export class PreguntaComponent implements OnInit {
   });
 
   crearPreguntaForm = new FormGroup({
-    numePregunta: new FormControl(''),
     enumPregunta: new FormControl('', [Validators.pattern(this.regTextoUnaLinea), Validators.maxLength(255)]),
     opcionA: new FormControl('', [Validators.pattern(this.regTextoUnaLinea), Validators.maxLength(80)]),
     opcionB: new FormControl('', [Validators.pattern(this.regTextoUnaLinea), Validators.maxLength(80)]),
@@ -48,6 +51,8 @@ export class PreguntaComponent implements OnInit {
   constructor(
     private preguntaService: PreguntaService,
     private toastrService: ToastrService,
+    private modalService: NgbModal,
+    private config: NgbModalConfig
   ) {
     this.listPregunta = [];
     this.seleccionEditar = new Pregunta();
@@ -55,6 +60,8 @@ export class PreguntaComponent implements OnInit {
     this.seleccionEditar.opciones[1] = new Opcion();
     this.seleccionEditar.opciones[2] = new Opcion();
     this.seleccionEditar.opciones[3] = new Opcion();
+    config.backdrop = 'static';
+    config.keyboard = false;
   }
 
   ngOnInit(): void {
@@ -108,6 +115,26 @@ export class PreguntaComponent implements OnInit {
   }
 
   resetearcrearPreguntaForm(){
-    
+
   }
+
+    /** Funciones para abrir y cerrar modal ng **/
+    open(content: any, tamaño: any) {
+
+      this.modalService.open(content, { ariaLabelledBy: 'modal-basic-title', size: tamaño, backdropClass: 'light-blue-backdrop', centered: true }).result.then((result) => {
+        this.closeResult = `Closed with: ${result}`;
+      }, (reason) => {
+        this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
+      });
+    }
+  
+    private getDismissReason(reason: any): string {
+      if (reason === ModalDismissReasons.ESC) {
+        return 'by pressing ESC';
+      } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
+        return 'by clicking on a backdrop';
+      } else {
+        return `with: ${reason}`;
+      }
+    }
 }
